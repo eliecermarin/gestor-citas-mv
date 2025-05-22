97cf4c906092fa19f412aa03bd303b6c7db1c1b0
eliecer.marin <eliecermarincuesta@gmail.com>
2025-05-22 17:04:10 +0200

    🛠️ Fix import supabaseClient desde src

--- src/pages/configuracion.tsx
+++ src/pages/configuracion.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../supabaseClient";
 import { useRouter } from "next/router";
 
 export default function Configuracion() {
