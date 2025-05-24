feac38c0a916dc72d4cba7530a792fa182168c14
eliecer.marin <eliecermarincuesta@gmail.com>
2025-05-24 10:39:47 +0200

    funcional supabase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import { useRouter } from 'next/router'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter()
+  
+  // Páginas que no necesitan layout
+  const noLayoutPages = ['/login']
+  const showLayout = !noLayoutPages.includes(router.pathname)
+
+  if (showLayout) {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
+  }
+
+  return <Component {...pageProps} />
 }
