3d24a07e4986318d827814ee394536afa6a2859d
eliecer.marin <eliecermarincuesta@gmail.com>
2025-05-24 13:43:24 +0200

    funcional supabase11

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -7,6 +9,14 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   transpilePackages: ['lucide-react'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@/lib': path.resolve(__dirname, 'src/lib'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
