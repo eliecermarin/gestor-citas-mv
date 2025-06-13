643c5b32f0750e15d5234259ad3256c7e9b7196f
eliecer.marin <eliecermarincuesta@gmail.com>
2025-06-13 16:46:34 +0200

    enviodeemailautomatico13

--- src/pages/api/test-simple.js
+++ src/pages/api/test-simple.js
@@ -1,5 +1,6 @@
 import { Resend } from 'resend';
 
+ // 🔥 LOG PARA VERIFICAR VERSIÓN DEL ARCHIVO
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
